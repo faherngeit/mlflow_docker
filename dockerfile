b8c1afa6839b3ca5bc4ac726e449e9df49501fb0
faherngeit <valeriy.tashchilin@hey.com>
2022-05-27 12:16:22 +0500

    Extend test log

--- dockerfile
+++ dockerfile
@@ -1,7 +1,3 @@
 FROM python:3.9
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-EXPOSE 5000
-COPY scripts/run.sh /
-RUN chmod +x /run.sh
-CMD ["/run.sh"]
--- test_log.py
+++ test_log.py
@@ -1,29 +1,50 @@
 import os
 from random import random, randint
 import mlflow
+import numpy as np
+from catboost import CatBoostClassifier
+from sklearn.metrics import roc_auc_score, log_loss
 
-if __name__ == "__main__":
+
+def log_mlflow():
     print("Running the test script ...")
 
+    # Set the experiment name#Connect to tracking server
+    mlflow.set_tracking_uri("http://127.0.0.1:12346")
 
+    mlflow.set_experiment("test_log")
+    mlflow.start_run()
 
-    #Connect to tracking server
-    mlflow.set_tracking_uri("http://127.0.0.1:8000")
+    train = np.random.rand(1000, 10)
+    test = np.random.rand(100, 10)
+    multiplier = np.random.rand(10, 1)
+    pre_target = train @ multiplier
+    pre_test = test @ multiplier
+    target = (pre_target > np.median(pre_target)).astype(int)
+    test_target = (pre_test > np.median(pre_test)).astype(int)
 
-    #Create directory for artifacts
-    if not os.path.exists("artifact_folder"):
-        os.makedirs("artifact_folder")
+    iterations = np.random.randint(20, 150)
+    model = CatBoostClassifier(iterations=iterations)
+    model.fit(train, target)
+    params = model.get_params()
+
+    train_pred = model.predict_proba(train)
+    test_pred = model.predict_proba(test)
+
+    stats = {}
+    stats['train_AUC'] = roc_auc_score(target, train_pred[:, 1])
+    stats['test_AUC'] = roc_auc_score(test_target, test_pred[:, 1])
+    stats['train_log_loss'] = log_loss(target, train_pred[:, 1])
+    stats['test_log_loss'] = log_loss(test_target, test_pred[:, 1])
 
     #Test parametes
-    mlflow.log_param("param1", randint(0, 100))
+    mlflow.log_param("iterations", params['iterations'])
 
     #Test metrics
-    mlflow.log_metric("metric1", random())
-    mlflow.log_metric("metric2", random())
-    mlflow.log_metric("metric3", random())
-    mlflow.log_metric("metric4", random())
+    mlflow.log_metrics(stats)
 
-    #Test artifact
-    with open("artifact_folder/test.txt", "w") as f:
-        f.write("hello world!")
-    # mlflow.log_artifacts("artifact_folder")
+    mlflow.catboost.log_model(model, "test_model")
+    mlflow.end_run()
+
+if __name__ == "__main__":
+    log_mlflow()
